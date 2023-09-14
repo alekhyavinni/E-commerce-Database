7f3c62d4ca0247de50918ceda197d49e162bae21
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-14 19:40:51 -0400

    find all tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,18 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+ Tag.findAll({
+  include:[
+    {
+      model:Product
+    }
+  ]
+ })
+ .then(dbData=>res.json(dbData))
+ .catch(err=>{
+  console.log(err)
+  res.status(500).json(err)
+ })
 });
 
 router.get('/:id', (req, res) => {
