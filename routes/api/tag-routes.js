0f193541d59da02c211546e85f9731426c2b3856
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-14 20:19:01 -0400

    destroy tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -71,6 +71,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(userDbData=>{
+    if(!userDbData){
+      res.status(404).json({message:'No Tag found with this id'})
+      return
+    }
+    res.json(userDbData)
+  })
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
