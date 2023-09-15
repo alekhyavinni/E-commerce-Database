2e26d0627c072947237f6bfd829c19a8b54fed01
bootcamp vinni <alekhyavinni12@gmail.com>
2023-09-14 20:10:39 -0400

    create tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -42,6 +42,14 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name:req.body.tag
+  })
+  .then(dbData=>res.json(dbData))
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.put('/:id', (req, res) => {
